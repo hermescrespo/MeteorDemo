95533d0a2a40d3aa4a125b1b15bedac1118f1e8a
hermescrespo <hermescrespo@gmail.com>
2016-05-17 11:45:22 -0400

    Creamos posts estáticos y de prueba Añadimos una plantilla básica para recorrer los posts

--- client/main.js
+++ client/main.js
@@ -1,22 +1,22 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // inicializando el contador en 0
-  this.contador = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  contadorTemplate() {
-    return Template.instance().contador.get();
-  }
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // incrementando el contador cuando el botón es presionado
-    instance.contador.set(instance.contador.get() + 1);
-  }
-});
+// import { Template } from 'meteor/templating';
+// import { ReactiveVar } from 'meteor/reactive-var';
+//
+// import './main.html';
+//
+// Template.hello.onCreated(function helloOnCreated() {
+//   // inicializando el contador en 0
+//   this.contador = new ReactiveVar(0);
+// });
+//
+// Template.hello.helpers({
+//   contadorTemplate() {
+//     return Template.instance().contador.get();
+//   }
+// });
+//
+// Template.hello.events({
+//   'click button'(event, instance) {
+//     // incrementando el contador cuando el botón es presionado
+//     instance.contador.set(instance.contador.get() + 1);
+//   }
+// });
---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Hermes Crespo on 16/05/2016.
+ */
+
+var postsData = [
+    {
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    },
+    {
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    },
+    {
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    }
+];
+Template.postsList.helpers({
+    posts: postsData
+});
