1d30ffadcee3582125e4698b189ed98f62950783
hermescrespo <hermescrespo@gmail.com>
2016-05-17 12:29:08 -0400

    Establecemos un ayudante `domain` en `postItem`.

---
+++ client/templates/posts/post_item.js
@@ -0,0 +1,12 @@
+/**
+ * Created by PlanificacionAdmin on 17/05/2016.
+ */
+
+Template.postItem.helpers({
+    domain: function() {
+        var a = document.createElement('a');
+        a.href = this.url;
+        return a.hostname;
+        // return this.url;
+    }
+});
