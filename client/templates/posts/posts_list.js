a6e834832f87a28f0a50b9e73bd55686b6531b2c
hermescrespo <hermescrespo@gmail.com>
2016-05-23 18:46:12 -0400

    Conexión entre la colección Posts y la plantilla `postList`.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -2,20 +2,22 @@
  * Created by Hermes Crespo on 16/05/2016.
  */
 
-var postsData = [
-    {
-        title: 'Introducing Telescope',
-        url: 'http://sachagreif.com/introducing-telescope/'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
+// var postsData = [
+//     {
+//         title: 'Introducing Telescope',
+//         url: 'http://sachagreif.com/introducing-telescope/'
+//     },
+//     {
+//         title: 'Meteor',
+//         url: 'http://meteor.com'
+//     },
+//     {
+//         title: 'The Meteor Book',
+//         url: 'http://themeteorbook.com'
+//     }
+// ];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function () {
+        return Posts.find();
+    }
 });
