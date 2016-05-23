1bc2232eef75b35267b7c6c6e7e3733402da6420
hermescrespo <hermescrespo@gmail.com>
2016-05-23 18:41:32 -0400

    Datos para la colección de posts.

---
+++ server/fixtures.js
@@ -0,0 +1,20 @@
+/**
+ * Created by PlanificacionAdmin on 23/05/2016.
+ */
+
+if (Posts.find().count() === 0) {
+    Posts.insert({
+        title: 'Introducing Telescope',
+        url: 'http://sachagreif.com/introducing-telescope/'
+    });
+
+    Posts.insert({
+        title: 'Meteor',
+        url: 'http://meteor.com'
+    });
+
+    Posts.insert({
+        title: 'The Meteor Book',
+        url: 'http://themeteorbook.com'
+    });
+}
