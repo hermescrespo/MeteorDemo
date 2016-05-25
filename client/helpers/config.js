6f9b2347185b48a44d2a204ac4c4a492904cfe45
hermescrespo <hermescrespo@gmail.com>
2016-05-25 18:39:28 -0400

    Añadidas cuentas de usuario y una plantilla en la cabecera

---
+++ client/helpers/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by VirtualSoft Academy on 25/05/2016.
+ */
+
+//Configurando para que los usuarios accedan al sistema a través de solo un nombre de usuario
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY'
+});
